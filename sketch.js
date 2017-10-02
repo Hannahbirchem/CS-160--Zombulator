d3ff337ea0299ded466056fe84c5bcd8bdf8742c
Hannah <birchem.hannah@gmail.com>
2017-10-02 09:52:19 -0700

    Added circles to sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 // Zombulator by Hannah Birchem 
 
 function setup() {
-
+ createcanvas(800, 800);
 }
 
 function draw() {
-
+	fill(255, 0, 0);
+ ellipse(50, 50, 80, 80);
+	fill(150, 150, 200);
+ ellipse(200, 100, 80, 80);
 }
-
