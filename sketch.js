1d582d3a8bfb8ed3a4fb2cced692d42a3642c501
Hannah <birchem.hannah@gmail.com>
2017-10-06 09:56:10 -0700

    Strob Circles

--- sketch.js
+++ sketch.js
@@ -1,16 +1,25 @@
 // Zombulator by Hannah Birchem 
 
 var zombiex = 50;
+var zombie2x = 100;
 
 function setup() {
-	createCanvas(800, 800);
+	createCanvas(displayWidth, displayHeight);
 }
 
 function draw() {
 	background(255, 255, 255);
-	fill(252, 252, 136);
+	fill(random(252), random(252), random(255));
+	strokeWeight(0);  // Default
 	ellipse(zombiex, 50, 80, 80);
-	fill(150, 150, 200);
-	ellipse(200, 100, 80, 80);
-	zombiex = zombiex + 10;
+	fill(random (150), random(150), random(200));
+	ellipse(zombie2x, 100, 80, 80);
+	zombiex = zombiex + 4;
+	zombie2x = zombie2x + 3;
+	if (zombiex >= width) {
+		zombiex = 0;
+	}
+	if (zombie2x >= width) {
+		zombie2x = 0;
+	}
 }
